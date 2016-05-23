1a470432be13444081f4011c9c4b5e7a80e12387
RobolinkInc <info@Robolink.com>
2016-05-24 03:13:36 +0900

    Version v1.2 library update

---
+++ examples/Trim/Controller_Flight_trim/Controller_Flight_trim.ino
@@ -0,0 +1,187 @@
+/*****************************************************************
+  Trim - Analog Joystick Control + Trim
+  This code will help you trim your CoDrone.
+
+- Touching the left sensor will send the Stop command to your drone
+- Covering the right sensor will engage Trim Mode
+- Covering both the left and right sensors will reset all trim values to 0
+
+How to Trim:
+When NOT covering the bottom right IR sensor, the controller will act as a normal controller. 
+Use this mode to test your CoDrone and see what trim is necassary.  If your drone tends to lean
+foward (pitch foward) then you will want to add a negative pitch trim.  If your drone tends to 
+lean diagonally to the back right (pitch backward and roll right) then you will want to add
+a positive pitch trim and a negative roll trim. 
+
+To add trim, simply cover the right IR sensor with your finger and push the joystick fully in the direction
+of the desired trim.  Trim adds incrementally, so if you want to add a lot of trim just tap the joystick 
+multiple times.  To properly add trim, you must be covering the right IR sensor while you move the joystick 
+in the trim direction.  The trim values will stay saved in the drone even after you turn off the controller 
+and change programs.
+
+Example:
+My drone does not fly straight up.  When I throttle upwards, it always tilts to the front and right. 
+--Solution--
+Upload this trim code.  Cover the right IR sensor with your finger and (while the sensor is still covered)
+tap the pitch joystick (right joystick by default) all the way to the bottom three times.  Uncover the right
+IR sensor.  Test the drone again by sending throttle (left joystick by default).  The drone now only tilts
+to the right (the negative pitch trim solved the forward tilt).  Cover the right IR sensor with your finger
+and tap the roll joystick (right joystick by default) all the way to the left three times.  Uncover the right
+IR sensor.  Test the drone again.  The drone now flies straight up and hovers without tilt.  Turn the drone
+and controller off, and upload any program you want. The trim will stay. 
+
+
+*******************************************************************/
+
+int PreviousEventPitch = 0;
+int PreviousEventThrottle = 0;
+int PreviousEventYaw = 0;
+int PreviousEventRoll = 0;
+
+#include <CoDrone.h> // Include the CoDrone library 
+
+void setup()
+{
+  CoDrone.begin(115200);                // Begin the serial connection with the CoDrone Bluetooth module(115200bps)
+  CoDrone.AutoConnect(NearbyDrone);     // Connect to a nearby drone
+  CoDrone.DroneModeChange(Flight);      // Ensure that the drone is in flight mode
+}
+
+void loop()
+{
+  byte bt1 = digitalRead(11);       // ■ □ □ □ □ □ □   Initialize the bottom left IR sensor
+  byte bt4 = digitalRead(14);       // □ □ □ ■ □ □ □   Initialize the bottom center IR sensor
+  byte bt8 = digitalRead(18);       // □ □ □ □ □ □ ■   Initialize the bottom right IR sensor
+
+  if (PAIRING == true)              // If the drone has succesfully paired
+  {
+
+    //**************************** STOP ******************************//
+    //■ □ □ □ □ □ □ □
+    if (bt1 && !bt4 && !bt8)           // If the left sensor is triggered
+    {
+      CoDrone.FlightEvent(Stop);      // Stop the CoDrone
+      // CoDrone.FlightEvent(Landing);      // Land the CoDrone.  You can choose either of these.
+    }
+
+    //*************************** Trim Reset ***********************//
+    //■ □ □ □ □ □ □ ■
+    else if (bt1 && !bt4 && bt8)  // If both the left and right sensors are triggered
+    {
+      CoDrone.Set_TrimReset();    // Reset all trim values
+      CoDrone.LED_Blink(25, 5);
+      CoDrone.LED_Connect();
+      CoDrone.ButtonPreesHoldWait(11, 18); //button up
+    }
+
+    //*************************** Control & Trim  ***********************//
+    //□ □ □ □ □ □ □ ■
+    if (!bt8)  //If the right sensor is NOT triggered
+    {
+      digitalWrite(16, HIGH);
+      YAW = -1 * CoDrone.AnalogScaleChange(analogRead(A3));   // Set the A3 analog pin to control the Yaw
+      THROTTLE  = CoDrone.AnalogScaleChange(analogRead(A4));  // Set the A4 analog pin to control the Throttle
+      ROLL = -1 * CoDrone.AnalogScaleChange(analogRead(A5));  // Set the A5 analog pin to control the Roll
+      PITCH = CoDrone.AnalogScaleChange(analogRead(A6));      // Set the A6 analog pin to control the Pitch
+      CoDrone.Control(SEND_INTERVAL);                         // Send the new flight commands at the SEND_INTERVAL (50ms)
+
+    }
+
+
+    else if (bt8)  // If the right sensor IS triggered
+    {
+      digitalWrite(16, LOW);
+      YAW = -1 * CoDrone.AnalogScaleChange(analogRead(A3));  //Set the joysticks to control the trim instead of the drone itself
+      THROTTLE  = CoDrone.AnalogScaleChange(analogRead(A4));
+      ROLL = -1 * CoDrone.AnalogScaleChange(analogRead(A5));
+      PITCH = CoDrone.AnalogScaleChange(analogRead(A6));
+
+      if (PITCH == 0) PreviousEventPitch = 0;                //Instead of using 'SEND_INTERVAL', we use the input values as triggers
+      if (THROTTLE == 0) PreviousEventThrottle = 0;
+      if (ROLL == 0) PreviousEventRoll = 0;
+      if (YAW == 0) PreviousEventYaw = 0;
+
+      if ((PITCH != 0) || (THROTTLE != 0) || (ROLL != 0) || (YAW != 0))
+      {
+        CoDrone.LED_Blink(20, 3);
+        CoDrone.LED_Connect();
+        digitalWrite(16, LOW);
+      }
+
+      if (PITCH > 90)                                    //If the Pitch value is high
+      {
+        if (PreviousEventPitch != PitchIncrease)         //Increase the Pitch Trim
+        {
+          PreviousEventPitch = PitchIncrease;
+          CoDrone.Set_Trim(PitchIncrease);
+        }
+      }
+
+      else if (PITCH < -90)                             //else if the Pitch value is low
+      {
+        if (PreviousEventPitch != PitchDecrease)        //Decrease the Pitch Trim
+        {
+          PreviousEventPitch = PitchDecrease;
+          CoDrone.Set_Trim(PitchDecrease);
+        }
+      }
+
+      else if (THROTTLE > 90)                           //And so on and so forth
+      {
+        if (PreviousEventThrottle != trim_ThrottleIncrease)
+        {
+          PreviousEventThrottle = trim_ThrottleIncrease;
+          CoDrone.Set_Trim(trim_ThrottleIncrease);
+        }
+      }
+
+      else if (THROTTLE < -90)
+      {
+        if (PreviousEventThrottle != trim_ThrottleDecrease)
+        {
+          PreviousEventThrottle = trim_ThrottleDecrease;
+          CoDrone.Set_Trim(trim_ThrottleDecrease);
+        }
+      }
+
+      else if (ROLL > 90)
+      {
+        if (PreviousEventRoll != RollDecrease)
+        {
+          PreviousEventRoll = RollDecrease;
+          CoDrone.Set_Trim(RollDecrease);
+        }
+      }
+      else if (ROLL < -90)
+      {
+        if (PreviousEventRoll != RollIncrease)
+        {
+          PreviousEventRoll = RollIncrease;
+          CoDrone.Set_Trim(RollIncrease);
+        }
+      }
+
+      else if (YAW > 90)
+      {
+        if (PreviousEventYaw != YawIncrease)
+        {
+          PreviousEventYaw = YawIncrease;
+          CoDrone.Set_Trim(YawIncrease);
+        }
+      }
+      else if (YAW < -90)                     //Cats are cooler than dogs
+      {
+        if (PreviousEventYaw != YawDecrease)
+        {
+          PreviousEventYaw = YawDecrease;
+          CoDrone.Set_Trim(YawDecrease);
+        }
+      }
+    }
+  }
+}
+
+
+
+
+
---
+++ examples/Trim/SerialTrimSet/SerialTrimSet.ino
@@ -0,0 +1,276 @@
+/*****************************************************************
+  Trim - Serial Trim Set
+
+This example will allow you to set your CoDrone trim using your serial monitor.
+
+- After uploading the code, keep your controller plugged into the computer and 
+open the Serial Monitor (ctrl + shift + M).
+- Set the baud rate to 115200 and the line ending to NewLine
+- After the CoDrone is paired, a list of commands and current trim values will appear
+on screen
+
+  Type these commands into the Serial input (at the top) to set your trim
+  help  : Display a list of commands
+  now   : Display current trim values
+  reset : Set all trim values to 0
+
+  To set a specific trim, type in the control and the amount:
+  :"roll 20" will set your roll trim to 20.  This will lean the drone to the right. 
+
+    ex) throttle -70
+        pitch 40
+        yaw -20
+
+  Values can range from -100 to 100
+
+  roll [value] : roll trim
+  yaw  [value] : yaw trim.
+  pitch [value]: pitch trim.
+  throttle [value] : throttle trim.
+
+*******************************************************************/
+
+#include <CoDrone.h> // Include CoDrone Library
+
+#define MAX_CMD_LENGTH  50
+char cmdBuff[MAX_CMD_LENGTH];
+int cmdIndex = 0;
+char* cmd;
+char* arg1;
+char* arg2;
+
+#define _Roll       1
+#define _Yaw        2
+#define _Throttle   3
+#define _Pitch      4
+#define _Wheel      5
+byte command = 0;
+
+int TrimAll_Roll;
+int TrimAll_Pitch;
+int TrimAll_Yaw;
+int TrimAll_Throttle;
+int TrimAll_Wheel;
+
+long ver = 160518;
+
+void setup()
+{
+  CoDrone.begin(115200);                // Begin serial connection (115200bps)
+  CoDrone.AutoConnect(NearbyDrone);    // Connect with a nearby drone
+
+  PrintTrimAll();                        // Display all trim values
+
+  delay(500);
+  CoDrone.Send_LinkModeBroadcast(LinkModeMute);
+  delay(500);
+
+  PrintMenu();
+  printPrompt();
+}
+
+void loop()
+{
+  if (Serial.available() > 0)
+  {
+    int input = Serial.read();
+    Serial.write(input); // echo
+    if (input == '\r')
+    {
+      input = '\n';
+      Serial.write(input); // echo
+    }
+    cmdBuff[cmdIndex++] = (char)input;
+    if (cmdIndex >= MAX_CMD_LENGTH)
+    {
+      cmdIndex = 0;
+      printMaxLengthError();
+    }
+    else
+    {
+      if (cmdBuff[cmdIndex - 1] == '\n')
+      {
+        cmdBuff[cmdIndex - 1] = 0; // end of string
+        cmd = strtok(cmdBuff, " "); // spilt command
+        arg1 = strtok(0, " "); // spilt argument1
+        arg2 = strtok(0, " "); // spilt argument2
+
+        if (strcmp(cmd, "help") == 0)
+          PrintMenu();
+
+        else if (strcmp(cmd, "now") == 0)
+        {
+          delay(500);
+          CoDrone.Send_LinkModeBroadcast(LinkBroadcast_Active);
+          PrintTrimAll();
+        }
+
+        else if (strcmp(cmd, "reset") == 0)
+          cmdTrimReset();
+
+        else if (strcmp(cmd, "roll") == 0)
+        {
+          command = _Roll;
+          cmdTrimSet(arg1);
+        }
+        else if (strcmp(cmd, "pitch") == 0)
+        {
+          command = _Pitch;
+          cmdTrimSet(arg1);
+        }
+        else if (strcmp(cmd, "yaw") == 0)
+        {
+          command = _Yaw;
+          cmdTrimSet(arg1);
+        }
+        else if (strcmp(cmd, "throttle") == 0)
+        {
+          command = _Throttle;
+          cmdTrimSet(arg1);
+        }
+        else if (strcmp(cmd, "wheel") == 0)
+        {
+          command = _Wheel;
+          cmdTrimSet(arg1);
+        }
+
+        else
+        {
+          if (cmdIndex - 1 == 0)
+            printPrompt();
+          else
+            printCmdError();
+        }
+        cmdIndex = 0;
+      }
+    }
+  }
+}
+
+void cmdTrimReset()
+{
+  delay(500);
+  CoDrone.Send_LinkModeBroadcast(LinkBroadcast_Active);
+  CoDrone.Set_TrimAll(0, 0, 0, 0, 0);
+  PrintTrimAll();
+}
+
+void cmdTrimSet(char* arg1)
+{
+  int _Trim = atoi(arg1);
+
+  delay(500);
+
+  CoDrone.Send_LinkModeBroadcast(LinkBroadcast_Active);
+
+  delay(500);
+  if (command == _Roll)
+    CoDrone.Set_TrimAll(_Trim, TrimAll_Pitch, TrimAll_Yaw, TrimAll_Throttle, TrimAll_Wheel);
+  else if (command == _Pitch)
+    CoDrone.Set_TrimAll(TrimAll_Roll, _Trim, TrimAll_Yaw, TrimAll_Throttle, TrimAll_Wheel);
+  else if (command == _Yaw)
+    CoDrone.Set_TrimAll(TrimAll_Roll, TrimAll_Pitch, _Trim, TrimAll_Throttle, TrimAll_Wheel);
+  else if (command == _Throttle)
+    CoDrone.Set_TrimAll(TrimAll_Roll, TrimAll_Pitch, TrimAll_Yaw, _Trim, TrimAll_Wheel);
+  else if (command == _Wheel)
+    CoDrone.Set_TrimAll(TrimAll_Roll, TrimAll_Pitch, TrimAll_Yaw, TrimAll_Throttle, _Trim);
+
+  command = 0;
+  PrintTrimAll();
+
+}
+
+
+void PrintMenu()
+{
+
+  while (Serial.available() > 0)
+  {
+    byte Temp = Serial.read();
+  }
+
+  Serial.println();
+  Serial.println("--------------- Trim Set Mode --------------- ");
+  Serial.print("- version = ");
+  Serial.println(ver);
+  Serial.println("- Serial Monitor Mode => Newline, 115200 baud");
+  Serial.println();
+  Serial.println("help");
+  Serial.println("now");
+  Serial.println("reset");
+  Serial.println("roll[value]\t EX : roll 20");
+  Serial.println("pitch[value]\t EX : pitch -20");
+  Serial.println("yaw[value]\t EX : yaw 20");
+  Serial.println("throttle[value]\t EX : throttle -20");
+  Serial.println("wheel[value]\t EX : wheel 20");
+  Serial.println("--------------------------------------------- ");
+
+  printPrompt();
+}
+
+void PrintTrimAll()
+{
+  delay(500);
+
+  CoDrone.Request_TrimAll();
+
+  CoDrone.PreviousMillis = millis();
+  while (!CoDrone.TimeCheck(1000))
+  {
+    CoDrone.Receive();
+  }
+
+  CoDrone.Send_LinkModeBroadcast(LinkModeMute);
+  delay(500);
+
+  TrimAll_Roll      = ((CoDrone.droneTrimAll[1] << 8) | (CoDrone.droneTrimAll[0]  & 0xff));
+  TrimAll_Pitch     = ((CoDrone.droneTrimAll[3] << 8) | (CoDrone.droneTrimAll[2]  & 0xff));
+  TrimAll_Yaw       = ((CoDrone.droneTrimAll[5] << 8) | (CoDrone.droneTrimAll[4]  & 0xff));
+  TrimAll_Throttle  = ((CoDrone.droneTrimAll[7] << 8) | (CoDrone.droneTrimAll[6]  & 0xff));
+  TrimAll_Wheel     = ((CoDrone.droneTrimAll[9] << 8) | (CoDrone.droneTrimAll[8]  & 0xff));
+
+  Serial.println("");
+  Serial.println("");
+  Serial.println("--------------- Now Trim ---------------");
+
+  Serial.print("roll \t\t");
+  Serial.println(TrimAll_Roll);
+
+  Serial.print("pitch \t\t");
+  Serial.println(TrimAll_Pitch);
+
+  Serial.print("yaw \t\t");
+  Serial.println(TrimAll_Yaw);
+
+  Serial.print("throttle \t");
+  Serial.println(TrimAll_Throttle);
+
+  Serial.print("wheel \t\t");
+  Serial.println(TrimAll_Wheel);
+  Serial.println("--------------------------------------------- ");
+
+  delay(1000);
+  while (Serial.available() > 0)
+  {
+    byte Temp = Serial.read();
+    //       Serial.println(Temp,HEX);
+  }
+  printPrompt();
+}
+
+void printCmdError()
+{
+  Serial.println("Bad command. type 'help'");
+  printPrompt();
+}
+void printPrompt()
+{
+  Serial.print("> ");
+}
+void printMaxLengthError()
+{
+  Serial.println();
+  Serial.println("Max command length");
+  printPrompt();
+}
+
