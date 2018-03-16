146fe810aa0d067f2ef3b3f40f27a8d72b88673c
“Robolink” <info@robolink.com>
2018-03-16 10:07:05 -0700

    add led default

--- CoDrone.cpp
+++ CoDrone.cpp
@@ -3448,30 +3448,121 @@ void CoDroneClass::setAllRGB(byte r, byte g, byte b)
 	setEyeRGB(r,g,b);
 }
 
+
+/*
+ *	function	:	setAllRGB(r, g, b)
+ *	description : 	setAllRGB(r, g, b) is function for change color for both arm and eye LED
+ *					It will remain after turn off and turn on again
+ *	param 		:
+ *					- r : int (0 ~ 255)
+ *					- g : int (0 ~ 255)
+ *					- b : int (0 ~ 255)
+ *	return 	 	:	none
+ */
 void CoDroneClass::setArmDefaultRGB(byte r, byte g, byte b)
 {
+	byte _packet[9];
+	byte _crc[2];
 
+	armred = r;
+	armgreen = g;
+	armblue = b;
+
+  	//header
+	_packet[0] = dType_LedDefaultModeColor;
+	_packet[1] = 5;
+
+ 	//data
+	_packet[2] = armmode;
+	_packet[3] = r;
+	_packet[4] = g;  
+	_packet[5] = b;  
+	_packet[6] = 100;
+
+	unsigned short crcCal = CRC16_Make(_packet, _packet[1]+2);
+	_crc[0] = (crcCal >> 8) & 0xff;
+	_crc[1] = crcCal & 0xff;
+
+	Send_Processing(_packet,_packet[1],_crc);
+	delay(250);
 }
 
+
+/*
+ *	function	:	setAllRGB(r, g, b)
+ *	description : 	setAllRGB(r, g, b) is function for change color for both arm and eye LED
+ *					It will remain after turn off and turn on again
+ *	param 		:
+ *					- r : int (0 ~ 255)
+ *					- g : int (0 ~ 255)
+ *					- b : int (0 ~ 255)
+ *	return 	 	:	none
+ */
 void CoDroneClass::setEyeDefaultRGB(byte r, byte g, byte b)
 {
+	byte _packet[9];
+	byte _crc[2];
 
+	armred = r;
+	armgreen = g;
+	armblue = b;
+
+  	//header
+	_packet[0] = dType_LedDefaultModeColor;
+	_packet[1] = 5;
+
+ 	//data
+	_packet[2] = eyemode;
+	_packet[3] = r;
+	_packet[4] = g;  
+	_packet[5] = b;  
+	_packet[6] = 100;
+
+	unsigned short crcCal = CRC16_Make(_packet, _packet[1]+2);
+	_crc[0] = (crcCal >> 8) & 0xff;
+	_crc[1] = crcCal & 0xff;
+
+	Send_Processing(_packet,_packet[1],_crc);
+	delay(250);
 }
 
+
+/*
+ *	function	:	setAllRGB(r, g, b)
+ *	description : 	setAllRGB(r, g, b) is function for change color for both arm and eye LED
+ *					It will remain after turn off and turn on again
+ *	param 		:
+ *					- r : int (0 ~ 255)
+ *					- g : int (0 ~ 255)
+ *					- b : int (0 ~ 255)
+ *	return 	 	:	none
+ */
 void CoDroneClass::setAllDefaultRGB(byte r, byte g, byte b)
 {
-
+	setArmDefaultRGB();
+	setEyeDefaultRGB();
 }
 
+
+/*
+ *	function	:	resetDefaultLED()
+ *	description : 	resetDefaultLED() is function for make LED default color and mode
+ *					It will change to red LED is hold
+ *	param 		:	none
+ *	return 	 	:	none
+ */
 void CoDroneClass::resetDefaultLED()
 {
-
+	setAllDefaultRGB(255,0,0);
+	setAllDefaultMode(LED_Hold);
 }
 
 
 /*
  *	function	:	setArmMode(mode)
  *	description : 	setArmMode(mode) is function for change mode for arm LED
+ *					Option is LED_None, LED_Hold, LED_Mix, LED_Flicker, 
+ *					LED_FlickerDouble, LED_Dimming, LED_Flow,LED_FlowReverse
  *	param 		:
  *					- r : mode
  *	return 	 	:	none
@@ -3502,8 +3593,10 @@ void CoDroneClass::setArmMode(byte mode)
 	delay(250);
 }
 /*
- *	function	:	setArmMode(mode)
- *	description : 	setArmMode(mode) is function for change mode for arm LED
+ *	function	:	setEyeMode(mode)
+ *	description : 	setEyeMode(mode) is function for change mode for eye LED
+ *					Option is LED_None, LED_Hold, LED_Mix, LED_Flicker, 
+ *					LED_FlickerDouble, LED_Dimming
  *	param 		:
  *					- r : mode
  *	return 	 	:	none
@@ -3535,8 +3628,10 @@ void CoDroneClass::setEyeMode(byte mode)
 }
 
 /*
- *	function	:	setArmMode(mode)
- *	description : 	setArmMode(mode) is function for change mode for arm LED
+ *	function	:	setAllMode(mode)
+ *	description : 	setAllMode(mode) is function for change mode for arm and eye LED
+ *					Option is LED_None, LED_Hold, LED_Mix, LED_Flicker, 
+ *					LED_FlickerDouble, LED_Dimming, LED_Flow,LED_FlowReverse
  *	param 		:
  *					- r : mode
  *	return 	 	:	none
@@ -3547,19 +3642,96 @@ void CoDroneClass::setAllMode(byte mode)
 	setEyeMode(mode);
 }
 
+
+/*
+ *	function	:	setArmDefaultMode(mode)
+ *	description : 	setArmDefaultMode(mode) is function for change mode for arm LED
+ *					Option is LED_None, LED_Hold, LED_Mix, LED_Flicker, 
+ *					LED_FlickerDouble, LED_Dimming, LED_Flow,LED_FlowReverse
+ *					It will remain after turn off and turn on again
+ *	param 		:
+ *					- r : mode
+ *	return 	 	:	none
+ */
 void CoDroneClass::setArmDefaultMode(byte mode)
 {
+	byte _packet[9];
+	byte _crc[2];
 
+	armmode = mode+0x30;
+
+  	//header
+	_packet[0] = dType_LedDefaultColor;
+	_packet[1] = 5;
+
+ 	//data
+	_packet[2] = armmode;
+	_packet[3] = armred;
+	_packet[4] = armgreen;  
+	_packet[5] = armblue;  
+	_packet[6] = 100;
+
+	unsigned short crcCal = CRC16_Make(_packet, _packet[1]+2);
+	_crc[0] = (crcCal >> 8) & 0xff;
+	_crc[1] = crcCal & 0xff;
+
+	Send_Processing(_packet,_packet[1],_crc);
+	delay(250);
 }
 
+
+/*
+ *	function	:	setEyeDefaultMode(mode)
+ *	description : 	setEyeDefaultMode(mode) is function for change mode for arm LED
+ *					Option is LED_None, LED_Hold, LED_Mix, LED_Flicker, 
+ *					LED_FlickerDouble, LED_Dimming
+ *					It will remain after turn off and turn on again
+ *	param 		:
+ *					- r : mode
+ *	return 	 	:	none
+ */
 void CoDroneClass::setEyeDefaultMode(byte mode)
 {
+	byte _packet[9];
+	byte _crc[2];
+
+	eyemode = mode;
+
+  	//header
+	_packet[0] = dType_LedDefaultColor;
+	_packet[1] = 5;
+
+ 	//data
+	_packet[2] = eyemode;
+	_packet[3] = eyered;
+	_packet[4] = eyegreen;  
+	_packet[5] = eyeblue;    
+	_packet[6] = 100;
+
+	unsigned short crcCal = CRC16_Make(_packet, _packet[1]+2);
+	_crc[0] = (crcCal >> 8) & 0xff;
+	_crc[1] = crcCal & 0xff;
+
+	Send_Processing(_packet,_packet[1],_crc);
+	delay(250);
 
 }
 
+
+/*
+ *	function	:	setAllDefaultMode(mode)
+ *	description : 	setAllDefaultMode(mode) is function for change mode for arm and eye LED
+ *					Option is LED_None, LED_Hold, LED_Mix, LED_Flicker, 
+ *					LED_FlickerDouble, LED_Dimming, LED_Flow,LED_FlowReverse
+ *					It will remain after turn off and turn on again
+ *	param 		:
+ *					- r : mode
+ *	return 	 	:	none
+ */
 void CoDroneClass::setAllDefaultMode(byte mode)
 {
-
+	setArmDefualtMode(mode);
+	setEyeDefualtMode(mode);
 }
 
 
